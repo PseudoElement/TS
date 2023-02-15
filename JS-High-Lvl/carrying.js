a03311c735e706807985126c55264a59d5c5d3e0
PseudoElement <bubamainer@mail.ru>
2023-02-15 19:07:46 +0300

    fix currying var

--- JS-High-Lvl/carrying.js
+++ JS-High-Lvl/carrying.js
@@ -1,21 +1,21 @@
-// let sum= function (...args1) {
-//     let total =args1.reduce((total,num) => total + num,0)
-//     return function(...args2) {
-//       if(args2.length!== 0) {
-//         let total2 = args2.reduce((total,num)=>total + num,0);
-//         return sum(total,total2);
-//     }
-//     return total;
-//   };
-//  };
-//  console.log(sum(2,3,4)(2,3)(1)())
-
-function sum(num1) {
-    return (num2) => {
-        if(!num2) {
-            return num1;///// для одного аргумента в каждой вызове
-        }
-        return sum(num1 + num2);
+let sum= function (...args1) {
+    let acc1 =args1.reduce((acc,num) => acc + num,0)
+    return function(...args2) {
+      if(args2.length!== 0) {
+        let acc2 = args2.reduce((acc,num)=>acc + num,0);
+        return sum(acc1,acc2);
     }
-}
-console.log('Sum :', sum(1)(2)(3)(4)(20)(6)(7)(8)())
+    return acc1;
+  };
+ };
+ console.log(sum(2,3,4)(2,3)(1)(22)())
+
+// function sum(num1) {
+//     return (num2) => {
+//         if(!num2) {
+//             return num1;///// для одного аргумента в каждой вызове
+//         }
+//         return sum(num1 + num2);
+//     }
+// }
+// console.log('Sum :', sum(1)(2)(3)(4)(20)(6)(7)(8)())
